6ea3f18a339e62470ae4216ace2d09bae2da9e04
JihyeJayPark <jihyepark1478@gmail.com>
2023-03-09 20:46:47 -0500

    successfully pulled value from dropdown menu for user's choice

--- app.js
+++ app.js
@@ -14,15 +14,34 @@ app.getData = () => {
         })
         .then(jsonResult => {
             console.log(jsonResult);
+            app.displayData(jsonResult);
         });
-
-
 }
 
-// Upon initiation, website will display a header reading: "SPORT SEEKER" and a short description: "Select your city and favourite sport from the dropdown menus below to view upcoming events in your area!"
 // A form that prompts users to select both a Location ("City") and Sport from two separate dropdown menus
-// Listen to the button click event below the form for Submission ('Go!)
+
+
 // Upon form submission, make an AJAX request to retrieve API data based on the user's form selections
+
+// create a method that displays data 
+app.displayData = (data) => {
+    // target form
+    app.form = document.querySelector('form');
+    // Listen to the button click event below the form for Submission ('Go!)
+    app.form.addEventListener('submit', function (e) {
+        // prevent the browser from refreshing (preventDefault)
+        e.preventDefault();
+        // store the user's choice
+        const location = document.getElementById('location').value;
+        const sport = document.getElementById('sport').value;
+    })
+    const ul = document.querySelector('.result');
+    // data.forEach((event) => {
+    //     const listElement = document.createElement('li');
+    // })
+}
+
+
 // Append results below Form Submit button
 // Display all upcoming sporting events in user's selected city
     // Team Names (who vs. who) - NAME
@@ -34,6 +53,7 @@ app.getData = () => {
 
 app.init = () => {
     app.getData();
+    app.displayData();
 }
 
 app.init();
